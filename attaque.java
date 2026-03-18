26bc2512263335edcb490afa8ba1d38f3f1ca0b8
adel <adelsnack@gmail.com>
2026-03-18 13:15:40 +0100

    ajout de attaque.java

---
+++ attaque.java
@@ -0,0 +1,29 @@
+public class attaque {
+    public int puissance;
+    public String nom;
+    public int precision;
+    public String type;
+    public int priorite;
+
+
+
+    public attaque(int puissance, String nom, int precision, String type, int priorite){
+        this.puissance = puissance;
+        this.nom = nom;
+        this.precision = precision;
+        this.type = type;
+        this.priorite = priorite;
+    }
+
+    public void Attaque(int puissance, int precision, String nom, Type type, int priorite){
+
+    
+    }
+
+    
+
+
+
+
+
+}
